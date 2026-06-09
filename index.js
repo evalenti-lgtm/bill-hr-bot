4a3d4df9ad7c8ba9af52b6f31983bf1a9543035d
evalenti-lgtm <evalenti@impossiblecloud.com>
2026-06-09 16:02:36 +0200

    index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY });
 async function searchClickUp(query) {
   try {
     const response = await fetch(
-      `https://api.clickup.com/api/v2/team/${process.env.CLICKUP_TEAM_ID}/space`,
+      `https://api.clickup.com/api/v2/space/49619321/folder`,
       {
         headers: {
           Authorization: process.env.CLICKUP_API_KEY,
